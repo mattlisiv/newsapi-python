5dd8208bf4c4d5735461b065564a6c17c1450bb9
404-brain <tyknott94@gmail.com>
2018-03-04 23:16:34 -0500

    Useful comments for parameters

--- newsapi/newsapi_client.py
+++ newsapi/newsapi_client.py
@@ -10,6 +10,33 @@ class NewsApiClient(object):
 
     def get_top_headlines(self, q=None, sources=None, language=None, country=None, category=None, page_size=None,
                           page=None):
+        """
+            Returns live top and breaking headlines for a country, specific category in a country, single source, or multiple 				sources..
+            
+            Optional parameters:
+                (str) q - return headlines w/ specified coin! Valid values are:
+                            'bitcoin', 'etheremum', 'ripple', 'bitcoin cash', etc.
+                            
+                (str) sources - return headlines of news sources! some Valid values are:
+                            'bbc-news', 'the-verge', 'abc-news', 'crypto coins news',
+                            'ary news','associated press','wired','aftenposten','australian financial review','axios',
+							'bbc news','bild','blasting news','bloomberg','business insider','engadget','google news',
+							'hacker news','info money,'recode','techcrunch','techradar','the next web','the verge' etc.
+							
+				(str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for. Valid values are:
+								'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+                            
+                (str) country - The 2-letter ISO 3166-1 code of the country you want to get headlines! Valid values are:
+                                'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr',    									'hk','hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz','ph' 									'pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us'
+								
+				(str) category - The category you want to get headlines for! Valid values are:
+								'business','entertainment','general','health','science','sports','technology'
+								
+				(int) page_size - The number of results to return per page (request). 20 is the default, 100 is the maximum. 
+								
+				(int) page - Use this to page through the results if the total results found is greater than the page size.										
+        """
+        
         # Define Payload
         payload = {}
         payload['q'] = q
@@ -26,6 +53,34 @@ class NewsApiClient(object):
 
     def get_everything(self, q=None, sources=None, domains=None, from_parameter=None, to=None, language=None,
                        sort_by=None, page=None, page_size=None):
+		"""
+            Search through millions of articles from over 5,000 large and small news sources and blogs.
+            
+            Optional parameters:
+                (str) q - return headlines w/ specified coin! Valid values are:
+                            'bitcoin', 'etheremum', 'ripple', 'bitcoin cash', etc.
+                            
+                (str) sources - return headlines of news sources! some Valid values are:
+                            'bbc-news', 'the-verge', 'abc-news', 'crypto coins news',
+                            'ary news','associated press','wired','aftenposten','australian financial review','axios',
+							'bbc news','bild','blasting news','bloomberg','business insider','engadget','google news',
+							'hacker news','info money,'recode','techcrunch','techradar','the next web','the verge' etc.
+							
+				(str) domains - A comma-seperated string of domains (eg bbc.co.uk, techcrunch.com, engadget.com) to restrict the 								search to. 
+				(str) from_parameter - A date and optional time for the oldest article allowed.(e.g. 2018-03-05 or 													2018-03-05T03:46:15)
+				
+				(str) to - A date and optional time for the newest article allowed.
+							
+				(str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for. Valid values are:
+								'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+								
+				(str) sort_by - The order to sort the articles in. Valid values are: relevancy, popularity, publishedAt.
+							relevancy
+								
+				(int) page_size - The number of results to return per page (request). 20 is the default, 100 is the maximum.		
+				(int) page - Use this to page through the results if the total results found is greater than the page size.										
+        """
+					   
         # Define Payload
         payload = {}
         payload['q'] = q
@@ -43,6 +98,23 @@ class NewsApiClient(object):
         return r.json()
 
     def get_sources(self, category=None, language=None, country=None):
+					   
+		 """
+            Returns the subset of news publishers that top headlines...
+            
+            Optional parameters:
+			
+				(str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for. Valid values are:
+								'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+                            
+                (str) country - The 2-letter ISO 3166-1 code of the country you want to get headlines! Valid values are:
+                                'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr',    									'hk','hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz','ph' 									'pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us'
+								
+				(str) category - The category you want to get headlines for! Valid values are:
+								'business','entertainment','general','health','science','sports','technology'
+																	
+        """			   
+					   
         # Define Payload
         payload = {}
         payload['category'] = category
