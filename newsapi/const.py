5ae68b7fe1d75ac6a7b3fbf8c42f7bcece8fbe45
menarguez <mamenarguez@gmail.com>
2019-07-26 15:35:57 -0700

    Adding new countries and languages that were missing
     - New countries: ["pk", "es", "is", "se", "zh"]
     - New languages:["se",
    "zh"])

--- newsapi/const.py
+++ newsapi/const.py
@@ -1,12 +1,12 @@
 TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines'
 EVERYTHING_URL = 'https://newsapi.org/v2/everything'
 SOURCES_URL = 'https://newsapi.org/v2/sources'
+countries = {'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','es','fr','gb','gr',
+             'hk','hu','id','ie','il','in','is','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz',
+             'ph','pk','pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us','ve','za',
+             'zh'}
 
-countries = {'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr','hk',
-             'hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz','ph','pl',
-             'pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us','ve','za'}
-
-languages = {'ar','en','cn','de','es','fr','he','it','nl','no','pt','ru','sv','ud'}
+languages = {'ar','en','cn','de','es','fr','he','it','nl','no','pt','ru','sv','se','ud','zh'}
 
 categories = {'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'}
 
