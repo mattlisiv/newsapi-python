bfcc1d96a9bd4669e17bf1791204bf7ed1b8f65b
Brad Solomon <brad.solomon.1124@gmail.com>
2019-09-06 11:16:30 -0400

    Update const.languages to use correct ISO-639-1
    "zh" and "se" are the correct codes,
    per https://newsapi.org/docs/endpoints/everything.

--- newsapi/const.py
+++ newsapi/const.py
@@ -6,7 +6,7 @@ countries = {'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','d
              'hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz','ph','pl',
              'pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us','ve','za'}
 
-languages = {'ar','en','cn','de','es','fr','he','it','nl','no','pt','ru','sv','ud'}
+languages = {'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'}
 
 categories = {'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'}
 
