e8861305d778664f258d64086e143d6d1a5cd5b5
Brad Solomon <brad.solomon.1124@gmail.com>
2019-09-07 18:50:56 -0400

    Update 'default' descriptions of const.py vars

--- newsapi/const.py
+++ newsapi/const.py
@@ -4,7 +4,8 @@ TOP_HEADLINES_URL = "https://newsapi.org/v2/top-headlines"
 EVERYTHING_URL = "https://newsapi.org/v2/everything"
 SOURCES_URL = "https://newsapi.org/v2/sources"
 
-#: The 2-letter ISO 3166-1 code of the country you want to get headlines for.  Default: all countries.
+#: The 2-letter ISO 3166-1 code of the country you want to get headlines for.  If not specified,
+#: the results span all countries.
 countries = {
     "ae",
     "ar",
@@ -66,11 +67,13 @@ countries = {
     "zh",
 }
 
-#: The 2-letter ISO-639-1 code of the language you want to get articles for.  Default: all languages.
+#: The 2-letter ISO-639-1 code of the language you want to get articles for.  If not specified,
+#: the results span all languages.
 languages = {"ar", "en", "cn", "de", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "sv", "se", "ud", "zh"}
 
-#: The category you want to get articles for.  Default: all categories.
+#: The category you want to get articles for.  If not specified,
+#: the results span all categories.
 categories = {"business", "entertainment", "general", "health", "science", "sports", "technology"}
 
-#: The order to sort article results in.  Default: publishedAt.
+#: The order to sort article results in.  If not specified, the default is ``"publishedAt"``.
 sort_method = {"relevancy", "popularity", "publishedAt"}
